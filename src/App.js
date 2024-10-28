38880dc840cda3d4791659a90d45e89a7d9c72ee
MrCooksIT <acoetzee@maristsj.co.za>
2024-10-28 13:36:32 +0200

    Fix 0.1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AssessmentPlatform from './components/AssessmentPlatform';
+
+function App() {
+  return (
+    <div className="App">
+      <AssessmentPlatform />
+    </div>
+  );
+}
+
+export default App;
